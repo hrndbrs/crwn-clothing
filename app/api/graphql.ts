0e09eff2c549f45665914753bf9f2f65cfb89285
hrndbrs <hrndbrs@gmail.com>
2024-01-14 14:02:59 +0700

    set up apollo server/client, created navigation component

---
+++ app/api/graphql.ts
@@ -0,0 +1,25 @@
+import { NextRequest } from "next/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { ApolloServer } from "@apollo/server";
+import { gql } from "graphql-tag";
+
+const resolvers = {
+	Query: {
+		hello: () => "world",
+	},
+};
+
+const typeDefs = gql`
+	type Query {
+		hello: String
+	}
+`;
+
+const server = new ApolloServer({
+	resolvers,
+	typeDefs,
+});
+
+const handler = startServerAndCreateNextHandler<NextRequest>(server);
+
+export { handler as GET, handler as POST };
---
+++ components/Navigation/Navigation.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+import CrwnLogo from "@/public/icons/crown.svg";
+import "./navigation.styles.scss";
+
+function Navigation() {
+	return (
+		<div className="navigation">
+			<Link className="logo-container" href="/">
+				<Image className="logo" src={CrwnLogo} alt="icon" />
+			</Link>
+			<div className="nav-links-container">
+				<Link className="nav-link" href="/shop">
+					SHOP
+				</Link>
+				<Link className="nav-link" href="/sign-in">
+					SIGN IN
+				</Link>
+			</div>
+		</div>
+	);
+}
+
+export default Navigation;
